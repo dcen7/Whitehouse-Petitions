0c2caeddd342d35c16aedf839e5a6b049ae73efc
Deniz <mdcengiz@gmail.com>
2020-05-09 08:56:01 -0500

    Add left bar button item

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -11,18 +11,68 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
-
+    var filteredItems = [Petition]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Rights", style: .plain, target: self, action: #selector(credits))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchPetitions))
+        
+        
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString){
             if let data = try? Data(contentsOf: url){
                 parse(json: data)
+                return
             }
             
         }
+        showError()
+    }
+    
+    @objc func credits(){
+        let ac = UIAlertController(title: "Data source",
+                                   message: "Data is recovered from We The People API of the Whitehouse",
+                                   preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func searchPetitions(){
+        let ac = UIAlertController(title: "Search petitions", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let searchAction = UIAlertAction(title: "Search", style: .default) { [weak self, weak ac]_ in
+            if let item = ac?.textFields?[0].text {
+                self?.filterPetitionsContaining(item)
+            }
+            
+        }
+        
+        ac.addAction(searchAction)
+        ac.addAction(UIAlertAction(title: "Clear current search", style: .default) { [weak self] _ in
+            self?.petitions = self!.filteredItems
+            self?.tableView.reloadData()
+        })
+        
+        ac.preferredAction = searchAction
+        present(ac, animated: true)
+        
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data){
@@ -30,6 +80,7 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
             petitions = jsonPetitions.results
+            filteredItems = petitions
             tableView.reloadData()
         }
     }
@@ -37,7 +88,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let petition = petitions[indexPath.row]
@@ -50,6 +101,15 @@ class ViewController: UITableViewController {
         let vc = DetailViewController()
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func filterPetitionsContaining(_ word: String){
+       
+        petitions = filteredItems.filter {
+            $0.title.localizedCaseInsensitiveContains(word)
+                || $0.body.localizedCaseInsensitiveContains(word)
+        }
+        tableView.reloadData()
     }
 }
 
